e63a24e4754fe24654d66829971bef707090a7c8
janeneos <janeneos@student.21-school.ru>
2026-05-26 18:22:06 +0300

    init commit

---
+++ src/test/java/org/example/stub/StubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
